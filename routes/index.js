16bddbad31a545cd5f6ec55fe3d156f2f641fbaa
Krishnadev P Melevila <krishnadev@localhost.localdomain>
2022-02-12 07:56:48 +0530

    chem aded

--- routes/index.js
+++ routes/index.js
@@ -189,7 +189,44 @@ router.post("/admin", async (req, res) => {
     console.log(err);
   }
 });
+router.post("/login", async (req, res) => {
+  // Our login logic starts here
+  try {
+    // Get user input
+    const { email, password } = req.body;
 
+    // Validate user input
+    if (!(email && password)) {
+      res.status(400).send("All input is required");
+    }
+    // Validate if user exist in our database
+    const api = await Api.findOne({ email });
+
+    if (api && (await bcrypt.compare(password, api.password))) {
+      // Create token
+      const token = jwt.sign(
+        { api_id: api._id, email },
+        process.env.TOKEN_KEY,
+        {
+          expiresIn: "2h",
+        }
+      );
+
+      // save user token
+      api.token = token;
+
+      // user
+      res.status(200).json(api);
+    }
+    else {
+      res.status(400).send("Invalid Credentials");
+    }
+
+  } catch (err) {
+    console.log(err);
+  }
+  // Our register logic ends here
+});
 router.get("/login", async (req, res) => {
   let user_token = req.cookies['x-access-token'];
 
